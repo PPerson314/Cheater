7580a770129919e10aec20760f0dad91c77bf0b9
Paul <paul.l.vantilburg@gmail.com>
2022-05-18 10:50:27 -0400

    used the cleaner <i> instead of <svg>

--- src/components/returnHome.svelte
+++ src/components/returnHome.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center items-center w-8 h-8 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 m-2">
     <a href="/">
-        <i class="iconoir-long-arrow-up-left"></i>
+        <i class="iconoir-long-arrow-up-left w-5/6 h-5/6"></i>
             
     </a>
 
