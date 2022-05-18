55dbf82552f01fb78a9405ddf8ba90d3444f6d94
Paul <paul.l.vantilburg@gmail.com>
2022-05-18 15:21:09 -0400

    small changes for formatting

--- src/components/returnHome.svelte
+++ src/components/returnHome.svelte
@@ -1,7 +1,5 @@
 <div class="flex justify-center items-center w-8 h-8 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 m-2">
     <a href="/">
         <i class="iconoir-long-arrow-up-left w-5/6 h-5/6"></i>
-            
     </a>
-
 </div>
