12aaa1fc528f004ccef3b51a395cdeb9d3fa074e
Paul <paul.l.vantilburg@gmail.com>
2022-05-18 15:25:18 -0400

    changed text color from text-black to text-gray-300

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 
 <div transition:slide="{{duration: 800}}">   
     <h1 class="text-4xl text-center my-8 uppercase">Home</h1>
-    <div class="grid gap-4 grid-cols-3">
+    <div class="grid gap-4 grid-cols-3 text-xl text-gray-300">
         {#each pages as page}
             <a class="bg-slate-600 rounded-md uppercase h-20 text-center p-5 m-2 text-lg" href="/{page}">{page}</a>
         {/each}
