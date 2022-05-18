005d778d19a168dd38008b905de7a24e75579216
Paul <paul.l.vantilburg@gmail.com>
2022-05-18 10:42:11 -0400

    Instead of using <svg> for the icon, directly imported the library to make the code cleaner

--- src/components/returnHome.svelte
+++ src/components/returnHome.svelte
@@ -1,9 +1,6 @@
 <div class="flex justify-center items-center w-8 h-8 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 m-2">
     <a href="/">
-        <svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.25 4.75L6.75 8.25L10.25 11.75" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M6.75 8.25L12.75 8.25C14.9591 8.25 16.75 10.0409 16.75 12.25V19.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        <i class="iconoir-long-arrow-up-left"></i>
             
     </a>
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,13 +2,14 @@
     import Nav from "../components/nav.svelte";
     import ReturnHome from "../components/returnHome.svelte";
 </script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lucaburgio/iconoir@master/css/iconoir.css">
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style> 
 
 <ReturnHome />
 <slot></slot> 
 <Nav />
-
-<style>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-</style> 
