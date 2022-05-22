fcdc0e1952aff28ed920464ee0ba29302f2d49ec
Paul <paul.l.vantilburg@gmail.com>
2022-05-22 17:58:20 -0400

    changed font size from lg to 2xl

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,11 @@
     const pages = ["calculator", "cat", "kinetic", "current", "potential", "acdc"]
 </script>
 
-<div transition:slide="{{duration: 800}}">   
+<div transition:slide="{{duration: 600}}">   
     <h1 class="text-4xl text-center my-8 uppercase">Home</h1>
     <div class="grid gap-4 grid-cols-3 text-xl text-gray-300">
         {#each pages as page}
-            <a class="bg-slate-600 rounded-md uppercase h-20 text-center p-5 m-2 text-lg" href="/{page}">{page}</a>
+            <a class="bg-slate-600 rounded-md uppercase h-20 text-center p-5 m-2 text-2xl" href="/{page}">{page}</a>
         {/each}
     </div>
 </div>
